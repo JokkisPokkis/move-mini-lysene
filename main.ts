fe4e689e1c63851dd0ddd55b731905aafe338d76
JokkisPokkis <119130210+JokkisPokkis@users.noreply.github.com>
2023-09-17 13:23:06 +0200

    Startfiler for MakeCode-prosjektet

---
+++ main.ts
@@ -0,0 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    lysene.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+    lysene.setPixelColor(1, neopixel.colors(NeoPixelColors.Yellow))
+    lysene.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+    lysene.setPixelColor(3, neopixel.colors(NeoPixelColors.Blue))
+    lysene.setPixelColor(4, neopixel.colors(NeoPixelColors.Purple))
+    lysene.show()
+})
+input.onButtonPressed(Button.B, function () {
+    lysene.clear()
+    lysene.show()
+})
+let lysene: neopixel.Strip = null
+lysene = neopixel.create(DigitalPin.P0, 5, NeoPixelMode.RGB)
+basic.forever(function () {
+	
+})
